d64151037259cb139e2f4cddd60a0c87d5d080e4
Emanuele Busi <emanuele.busi@mondora.com>
2016-07-07 18:27:20 +0200

    Used upsert instead of insert, to avoid issues if a same insert event is sent multiple times in a short period

--- src/services/mongodb.js
+++ src/services/mongodb.js
@@ -5,18 +5,15 @@ import {MONGODB_URL} from "../config";
 export default MongoClient.connect(MONGODB_URL);
 
 export async function insert (collectionName, sensor, id) {
-    const db = await MongoClient.connect(MONGODB_URL);
-    sensor._id = id;
-    return db.collection(collectionName).insert(
-        sensor
-    );
+    return update(collectionName, sensor, id);
 }
 
 export async function update (collectionName, sensor, id) {
     const db = await MongoClient.connect(MONGODB_URL);
     return db.collection(collectionName).update(
         {_id: id},
-        {$set: sensor}
+        {$set: sensor},
+        {upsert: true}
     );
 }
 
